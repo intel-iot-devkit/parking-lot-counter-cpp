0bf3cdaad55e519551689174ae1326156d0cc4d6
Milos Gajdos <milosgajdos83@gmail.com>
2018-09-22 14:01:20 +0100

    First commit. Causes Inference Engine coredump

---
+++ README.md
@@ -0,0 +1,146 @@
+# Factory Virtual Fence
+
+| Details            |              |
+|-----------------------|---------------|
+| Target OS:            |  Ubuntu\* 16.04 LTS   |
+| Programming Language: |  C++\* |
+| Time to Complete:     |  45 min     |
+
+![app image](./images/parking-space-monitor.png)
+
+## Introduction
+
+This parking space monitor is one of a series of reference implementations for Computer Vision (CV) using the OpenVINO™ toolkit. This application is designed for a parking space area mounted camera which monitors available parking space by tracking the counts of the vehicles entering and leaving the parking space area.
+
+This example is intended to demonstrate how to use CV to monitor parking spaces in dedicated parking area;
+
+## Requirements
+
+### Hardware
+* 6th Generation Intel® Core™ processor with Intel® Iris® Pro graphics and Intel® HD Graphics
+
+### Software
+* [Ubuntu\* 16.04 LTS](http://releases.ubuntu.com/16.04/)
+*Note*: You must be running kernel version 4.7+ to use this software. We recommend using a 4.14+ kernel to use this software. Run the following command to determine your kernel version:
+```
+uname -a
+```
+* OpenCL™ Runtime Package
+* OpenVINO™ toolkit
+
+## Setup
+
+### Install OpenVINO™ Toolkit
+Refer to https://software.intel.com/en-us/articles/OpenVINO-Install-Linux for more information about how to install and setup the OpenVINO™ toolkit.
+
+You will need the OpenCL™ Runtime package if you plan to run inference on the GPU as shown by the
+instructions below. It is not mandatory for CPU inference.
+
+## How it works
+
+The application uses a video source, such as a camera, to grab frames, and then uses a Deep Neural Network (DNNs) to process the data. The network detects vehicles in the frame, and then if successful it tracks the vehicles leaving and entering the parking area adjusting the counts of the vehicles in the parking area thus providing the information about the available parking spaces.
+
+The data can then optionally be sent to a MQTT machine to machine messaging server, as part of a parking space data analytics system.
+
+The DNN models used in this application are Intel® optimized models that are part of the OpenVINO™ toolkit.
+
+You can find them here:
+
+- `/opt/intel/computer_vision_sdk/deployment_tools/intel_models/vehicle-license-plate-detection-barrier-0106`
+
+![Code organization](./images/arch3.png)
+
+The program creates three threads for concurrency:
+
+- main thread that performs the video i/o
+- worker thread that processes video frames using the deep neural networks
+- worker thread that publishes any MQTT messages
+
+## Setting the build environment
+
+You must configure the environment to use the OpenVINO™ toolkit one time per session by running the following command:
+```
+    source /opt/intel/computer_vision_sdk/bin/setupvars.sh
+```
+
+## Building the code
+
+Start by changing the current directory to wherever you have git cloned the application code. For example:
+```
+    cd available-parking-tracker-cpp
+```
+
+If you do not yet have a `build` directory create one:
+```
+    mkdir build
+```
+
+Then change to that directory:
+```
+    cd build
+```
+
+Now run the following commands:
+```
+    cmake ..
+    make
+```
+
+Once the commands are finished, you should have built the `monitor` application executable.
+
+## Running the code
+
+To see a list of the various options:
+```
+    ./monitor -h
+```
+
+To run the application with the needed models using the webcam:
+```
+    ./monitor -m=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/vehicle-license-plate-detection-barrier-0106/FP32/vehicle-license-plate-detection-barrier-0106.bin -c=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/vehicle-license-plate-detection-barrier-0106/FP32/vehicle-license-plate-detection-barrier-0106.xml
+```
+
+### Hardware acceleration
+
+This application can take advantage of the hardware acceleration in the OpenVINO toolkit by using the `-b` and `-t` parameters.
+
+For example, to use the OpenVINO™ toolkit backend with the GPU in 32-bit mode:
+```
+    ./monitor -m=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/vehicle-license-plate-detection-barrier-0106/FP32/vehicle-license-plate-detection-barrier-0106.bin -c=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/vehicle-license-plate-detection-barrier-0106/FP32/vehicle-license-plate-detection-barrier-0106.xml -b=2 -t=1
+```
+
+To run the code using 16-bit floats, you have to both set the `-t` flag to use the GPU in 16-bit mode, as well as use the FP16 version of the Intel® models:
+```
+    ./monitor -m=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/vehicle-license-plate-detection-barrier-0106/FP16/vehicle-license-plate-detection-barrier-0106.bin -c=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/vehicle-license-plate-detection-barrier-0106/FP16/vehicle-license-plate-detection-barrier-0106.xml -b=2 -t=2
+```
+
+## Sample videos
+
+There are several videos available to use as sample videos to show the capabilities of this application. You can download them by running these commands from the `factory-virtual-fence-cpp` directory:
+```
+    mkdir resources
+    cd resources
+    wget https://github.com/intel-iot-devkit/sample-videos/raw/master/car-detection.mp4
+    cd ..
+```
+
+To then execute the code using one of these sample videos, run the following commands from the `factory-virtual-fence-cpp` directory:
+```
+    cd build
+    ./monitor -m=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/vehicle-license-plate-detection-barrier-0106/FP32/vehicle-license-plate-detection-barrier-0106.bin -c=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/vehicle-license-plate-detection-barrier-0106/FP32/vehicle-license-plate-detection-barrier-0106.xml -i=../resources/car-detection.mp4
+```
+
+### Machine to machine messaging with MQTT
+
+If you wish to use a MQTT server to publish data, you should set the following environment variables before running the program:
+```
+    export MQTT_SERVER=localhost:1883
+    export MQTT_CLIENT_ID=cvservice
+```
+
+Change the `MQTT_SERVER` to a value that matches the MQTT server you are connecting to.
+
+You should change the `MQTT_CLIENT_ID` to a unique value for each monitoring station, so you can track the data for individual locations. For example:
+```
+    export MQTT_CLIENT_ID=factory1337
+```
---
+++ src/main.cpp
@@ -0,0 +1,376 @@
+/*
+* Copyright (c) 2018 Intel Corporation.
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+* LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+// std includes
+#include <iostream>
+#include <stdio.h>
+#include <thread>
+#include <queue>
+#include <map>
+#include <atomic>
+#include <csignal>
+#include <ctime>
+#include <mutex>
+#include <syslog.h>
+
+// OpenCV includes
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/dnn.hpp>
+
+// MQTT
+#include "mqtt.h"
+
+using namespace std;
+using namespace cv;
+using namespace dnn;
+
+// OpenCV-related variables
+Mat frame, blob;
+VideoCapture cap;
+int delay = 5;
+Net net;
+
+// application parameters
+String model;
+String config;
+int backendId;
+int targetId;
+int rate;
+
+// flag to control background threads
+atomic<bool> keepRunning(true);
+
+// flag to handle UNIX signals
+static volatile sig_atomic_t sig_caught = 0;
+
+// mqtt parameters
+const string topic = "parking/counter";
+
+// ParkingInfo contains information about assembly line
+struct ParkingInfo
+{
+    int count;
+};
+
+// currentInfo contains the latest ParkingInfo as tracked by the application
+ParkingInfo currentInfo = {0};
+
+// nextImage provides queue for captured video frames
+queue<Mat> nextImage;
+String currentPerf;
+
+mutex m, m1, m2;
+
+const char* keys =
+    "{ help     | | Print help message. }"
+    "{ device d    | 0 | camera device number. }"
+    "{ input i     | | Path to input image or video file. Skip this argument to capture frames from a camera. }"
+    "{ model m     | | Path to .bin file of model containing face recognizer. }"
+    "{ config c    | | Path to .xml file of model containing network configuration. }"
+    "{ backend b   | 0 | Choose one of computation backends: "
+                        "0: automatically (by default), "
+                        "1: Halide language (http://halide-lang.org/), "
+                        "2: Intel's Deep Learning Inference Engine (https://software.intel.com/openvino-toolkit), "
+                        "3: OpenCV implementation }"
+    "{ target t    | 0 | Choose one of target computation devices: "
+                        "0: CPU target (by default), "
+                        "1: OpenCL, "
+                        "2: OpenCL fp16 (half-float precision), "
+                        "3: VPU }"
+    "{ rate r      | 1 | number of seconds between data updates to MQTT server. }";
+
+// nextImageAvailable returns the next image from the queue in a thread-safe way
+Mat nextImageAvailable() {
+    Mat rtn;
+    m.lock();
+    if (!nextImage.empty()) {
+        rtn = nextImage.front();
+        nextImage.pop();
+    }
+    m.unlock();
+
+    return rtn;
+}
+
+// addImage adds an image to the queue in a thread-safe way
+void addImage(Mat img) {
+    m.lock();
+    if (nextImage.empty()) {
+        nextImage.push(img);
+    }
+    m.unlock();
+}
+
+// getCurrentInfo returns the most-recent ParkingInfo for the application.
+ParkingInfo getCurrentInfo() {
+    m2.lock();
+    ParkingInfo info;
+    info = currentInfo;
+    m2.unlock();
+
+    return info;
+}
+
+// updateInfo uppdates the current ParkingInfo for the application to the latest detected values
+void updateInfo(ParkingInfo info) {
+    m2.lock();
+    currentInfo.count = info.count;
+    m2.unlock();
+}
+
+// resetInfo resets the current ParkingInfo for the application.
+void resetInfo() {
+    m2.lock();
+    currentInfo.count = 0;
+    m2.unlock();
+}
+
+// getCurrentPerf returns a display string with the most current performance stats for the Inference Engine.
+string getCurrentPerf() {
+    string perf;
+    m1.lock();
+    perf = currentPerf;
+    m1.unlock();
+
+    return perf;
+}
+
+// savePerformanceInfo sets the display string with the most current performance stats for the Inference Engine.
+void savePerformanceInfo() {
+    m1.lock();
+
+    vector<double> times;
+    double freq = getTickFrequency() / 1000;
+    double t = net.getPerfProfile(times) / freq;
+
+    string label = format("Car inference time: %.2f ms", t);
+
+    currentPerf = label;
+
+    m1.unlock();
+}
+
+// publish MQTT message with a JSON payload
+void publishMQTTMessage(const string& topic, const ParkingInfo& info)
+{
+    ostringstream s;
+    s << "{\"Count\": \"" << info.count << "\"}";
+    string payload = s.str();
+
+    mqtt_publish(topic, payload);
+
+    string msg = "MQTT message published to topic: " + topic;
+    syslog(LOG_INFO, "%s", msg.c_str());
+    syslog(LOG_INFO, "%s", payload.c_str());
+}
+
+// message handler for the MQTT subscription for the any desired control channel topic
+int handleMQTTControlMessages(void *context, char *topicName, int topicLen, MQTTClient_message *message)
+{
+    string topic = topicName;
+    string msg = "MQTT message received: " + topic;
+    syslog(LOG_INFO, "%s", msg.c_str());
+
+    return 1;
+}
+
+// Function called by worker thread to process the next available video frame.
+void frameRunner() {
+    while (keepRunning.load()) {
+        Mat next = nextImageAvailable();
+        if (!next.empty()) {
+            // convert to 4d vector as required by face detection model, and detect cars
+            blobFromImage(next, blob, 1.0, Size(300, 300));
+            net.setInput(blob);
+            cout << "STOP FRAME_RUNNER HERE TO WAIT FOR THE MAIN THREAD" << endl;
+            this_thread::sleep_for(std::chrono::milliseconds(1000));
+            cout << "COREDUMPS HERE: " << &net << endl;
+            Mat result = net.forward();
+
+            // get detected cars
+            vector<Rect> cars;
+
+            float* data = (float*)result.data;
+            for (size_t i = 0; i < result.total(); i += 7)
+            {
+                float confidence = data[i + 2];
+                if (confidence > 0.5)
+                {
+                    int label = (int)data[i + 1];
+                    int left = (int)(data[i + 3] * frame.cols);
+                    int top = (int)(data[i + 4] * frame.rows);
+                    int right = (int)(data[i + 5] * frame.cols);
+                    int bottom = (int)(data[i + 6] * frame.rows);
+                    int width = right - left + 1;
+                    int height = bottom - top + 1;
+
+                    cars.push_back(Rect(left, top, width, height));
+                }
+            }
+
+            // detect if there are any people in marked area
+            for(auto const& c: cars) {
+                // TODO: this will have to change to tracking car center
+                // make sure the person rect is completely inside the main Mat
+                //if ((c & Rect(0, 0, next.cols, next.rows)) != c) {
+                //    continue;
+                //}
+
+                // TODO: figure out the counting here
+                //rectangle(next, c, (0, 255, 0), 2);
+            }
+
+            // operator data
+            ParkingInfo info;
+            info.count = cars.size();
+
+            updateInfo(info);
+            savePerformanceInfo();
+        }
+    }
+
+    cout << "Video processing thread stopped" << endl;
+}
+
+// Function called by worker thread to handle MQTT updates. Pauses for rate second(s) between updates.
+void messageRunner() {
+    while (keepRunning.load()) {
+        ParkingInfo info = getCurrentInfo();
+        publishMQTTMessage(topic, info);
+        this_thread::sleep_for(chrono::seconds(rate));
+    }
+
+    cout << "MQTT sender thread stopped" << endl;
+}
+
+// signal handler for the main thread
+void handle_sigterm(int signum)
+{
+    /* we only handle SIGTERM and SIGKILL here */
+    if (signum == SIGTERM) {
+        cout << "Interrupt signal (" << signum << ") received" << endl;
+        sig_caught = 1;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    // parse command parameters
+    CommandLineParser parser(argc, argv, keys);
+    parser.about("Use this script to using OpenVINO.");
+    if (argc == 1 || parser.has("help"))
+    {
+        parser.printMessage();
+
+        return 0;
+    }
+
+    model = parser.get<String>("model");
+    config = parser.get<String>("config");
+    backendId = parser.get<int>("backend");
+    targetId = parser.get<int>("target");
+    rate = parser.get<int>("rate");
+
+    // connect MQTT messaging
+    int result = mqtt_start(handleMQTTControlMessages);
+    if (result == 0) {
+        syslog(LOG_INFO, "MQTT started.");
+    } else {
+        syslog(LOG_INFO, "MQTT NOT started: have you set the ENV varables?");
+    }
+
+    mqtt_connect();
+
+    // open face model
+    net = readNet(model, config);
+    net.setPreferableBackend(backendId);
+    net.setPreferableTarget(targetId);
+
+    // open video capture source
+    if (parser.has("input")) {
+        cap.open(parser.get<String>("input"));
+
+        // also adjust delay so video playback matches the number of FPS in the file
+        double fps = cap.get(CAP_PROP_FPS);
+        delay = 1000/fps;
+    }
+    else
+        cap.open(parser.get<int>("device"));
+
+    if (!cap.isOpened()) {
+        cerr << "ERROR! Unable to open video source\n";
+        return -1;
+    }
+
+    // register SIGTERM signal handler
+    signal(SIGTERM, handle_sigterm);
+
+    // start worker threads
+    thread t1(frameRunner);
+    thread t2(messageRunner);
+
+    // read video input data
+    for (;;) {
+        cap.read(frame);
+
+        if (frame.empty()) {
+            keepRunning = false;
+            cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+
+        addImage(frame);
+
+        // print Inference Engine performance info
+        string label = getCurrentPerf();
+        putText(frame, label, Point(0, 15), FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(0, 0, 255));
+
+        ParkingInfo info = getCurrentInfo();
+        label = format("Car Count: %d", info.count);
+        putText(frame, label, Point(0, 40), FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(0, 0, 255));
+
+        cout << "STOP MAIN_THREAD TO WAIT FOR INFERENCE TO KICK IN" << endl;
+        this_thread::sleep_for(std::chrono::milliseconds(2000));
+        imshow("Parking Tracker", frame);
+
+        if (waitKey(delay) >= 0 || sig_caught) {
+            cout << "Attempting to stop background threads" << endl;
+            keepRunning = false;
+            break;
+        }
+    }
+
+    // wait for the threads to finish
+    t1.join();
+    t2.join();
+    cap.release();
+
+    // disconnect MQTT messaging
+    mqtt_disconnect();
+    mqtt_close();
+
+    return 0;
+}
